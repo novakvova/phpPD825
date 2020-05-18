165d660909b5016c5594bf2c3a68be7adda56366
novakvova <novakvova@gmail.com>
2020-05-18 16:26:21 +0300

    PHP Begin

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once("style.php"); ?>
+    <title>Document</title>
+</head>
+<body>
+<?php include_once("navbar.php"); ?>
+
+<div class="container">
+    <div class="row">
+        <h1>Овочі</h1>
+    </div>
+</div>
+
+
+
+<?php include_once("scripts.php"); ?>
+
+</body>
+</html>
---
+++ scripts.php
@@ -0,0 +1,4 @@
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/popper.js/dist/popper.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
