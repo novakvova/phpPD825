dbd4fe6e6e5dc6ef7b746fbc54fc46a86bfcd921
novakvova <novakvova@gmail.com>
2020-05-19 16:05:57 +0300

    Good app

--- register.php
+++ register.php
@@ -3,19 +3,47 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 {
     $email = $_POST["email"];
     $password = $_POST['password'];
-    $user="lopatkin";
-    $pass="123456";
-    $dbh = new PDO('mysql:host=localhost;dbname=dblopatkin', $user, $pass);
-
-    $sql = "INSERT INTO `tbl_users` (`email`, `password`, `image`) VALUES (?, ?, 'nophoto');";
-    $stmt= $dbh->prepare($sql);
-    $stmt->execute([$email, $password]);
-    header("Location:  index.php");
-    exit();
-    echo "<script>alert('POST JS".$email."'); </script>";
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    if(!$uppercase || !$lowercase || !$number || strlen($password) < 8) {
+        $error="Занадто слабкий пароль";
+    }else{
+        $error="";
+        $user="lopatkin";
+        $pass="123456";
+        $dbh = new PDO('mysql:host=localhost;dbname=dblopatkin', $user, $pass);
+        $sql = "SELECT u.id FROM tbl_users AS u WHERE u.email=? LIMIT 1";
+        $stmt= $dbh->prepare($sql);
+        $stmt->execute([$email]);
+        if($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            $error="Данний юзер вже зареєстрований";
+        }
+//        while ($row = $stmt->fetch()) {
+//            if($row['email']==$email){
+//                $error="Данний юзер вже зареєстрований";
+//            }
+//            //echo $row['email']."<br />\n";
+//        }
+        if($error=="")
+        {
+            $sql = "INSERT INTO `tbl_users` (`email`, `password`, `image`) VALUES (?, ?, 'nophoto');";
+            $stmt= $dbh->prepare($sql);
+            $stmt->execute([$email, $password]);
+            echo "<script>console.log('wwdwdw')</script>";
+            header("Location:  index.php");
+            exit();
+        }
+        //echo "<script>alert('POST JS".$email."'); </script>";
+    }
+}
+else{
+    $email="";
+    $password="";
+    $error="";
 }
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -23,35 +51,38 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php include_once("style.php"); ?>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-
-<?php include_once("navbar.php"); ?>
-
+<?php include("navbar.php");?>
 <div class="container">
     <div class="row">
         <h1 class="col-12 text-center">Реєстрація</h1>
     </div>
     <div class="row">
-        <form class="col-12" action="register.php" method="post" enctype="multipart/form-data">
+        <form class="col-12 " action="register.php" method="post" enctype="multipart/form-data">
+            <label class="offset-3 col-6 " style="color: red"><?php echo $error ?></label>
             <div class="offset-3 col-6 form-group">
                 <label for="email">Електронна пошта</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                <input required type="email" class="form-control" id="email" name="email" value="<?php echo $email ?>" aria-describedby="emailHelp">
             </div>
             <div class="offset-3 col-6 form-group">
                 <label for="password">Пароль</label>
-                <input type="password" class="form-control" id="password" name="password">
+                <input required type="password" value="<?php echo $password ?>" class="form-control" id="password" name="password">
             </div>
-            <button type="submit" class="offset-4 btn btn-primary">Реєстрація</button>
+            <div class="offset-3 form-group form-check">
+                <input required type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Я буду ходити в магазин в масці</label>
+            </div>
+            <button type="submit" class="offset-8 btn btn-primary">Реєстрація</button>
         </form>
     </div>
 </div>
 
-
-
-<?php include_once("scripts.php"); ?>
-
+<script src="node_modules/jquery/dist/jquery.min.js" />
+<script src="node_modules/popper.js/dist/popper.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
+
