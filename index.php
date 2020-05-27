8a11011bbe99854833aebef2475499529172c1eb
novakvova <novakvova@gmail.com>
2020-05-27 18:14:18 +0300

    Update home page

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $stmt->execute();
 
 <div class="container">
     <div class="row">
-        <h1>Овочі</h1>
+        <h1>Веселі користувачі</h1>
         <table class="table table-dark">
             <thead>
             <tr>
